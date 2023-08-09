10e7c22384731f2356cec4e6f6cf9e0a4f1a8d54
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-09 17:37:16 +0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,34 +8,35 @@ import DesktopLine2 from "../assets/DesktopLine2.png";
 import Accordion from "./Accordion";
 import { Fragment } from "react";
 import { useState } from "react";
+import { AccordionItemsArray } from "./AccordionItemsArray";
 
-const AccordionItemsArray = [
-  {
-    question: "How many team members can I invite?",
-    answer:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-  {
-    question: "What is the maximum file upload size?",
-    answer:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-  {
-    question: "How do I reset my password?",
-    answer:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-  {
-    question: "Can I cancel my subscription?",
-    answer:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-  {
-    question: "Do you provide additional support?",
-    answer:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-];
+// const AccordionItemsArray = [
+//   {
+//     question: "How many team members can I invite?",
+//     answer:
+//       "No more than 2GB. All files in your account must fit your allotted storage space.",
+//   },
+//   {
+//     question: "What is the maximum file upload size?",
+//     answer:
+//       "No more than 2GB. All files in your account must fit your allotted storage space.",
+//   },
+//   {
+//     question: "How do I reset my password?",
+//     answer:
+//       "No more than 2GB. All files in your account must fit your allotted storage space.",
+//   },
+//   {
+//     question: "Can I cancel my subscription?",
+//     answer:
+//       "No more than 2GB. All files in your account must fit your allotted storage space.",
+//   },
+//   {
+//     question: "Do you provide additional support?",
+//     answer:
+//       "No more than 2GB. All files in your account must fit your allotted storage space.",
+//   },
+// ];
 
 const Header = () => {
   const [visible, setVisible] = useState<string>("");
